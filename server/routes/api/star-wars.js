d4b51d7f580addd7e1136a200a099e567962f6ef
Joshua Chin <joshua.chin21@gmail.com>
2019-02-26 06:03:51 -0800

    1. updated GET Endpoint to take search query and pass to SWAPI

--- server/routes/api/star-wars.js
+++ server/routes/api/star-wars.js
@@ -6,37 +6,48 @@ const router = express.Router();
 
 const url = 'https://swapi.co/api/people/';
 
-router.get('/', (req, res) =>
-  Rx.Observable.create((observer) => {
-      request(
-        url,
+/**
+ *
+ * @param baseUrl
+ * @param req
+ * @returns {string}
+ */
+const getUrl = (baseUrl, req) => {
+    return req.query && req.query.search ? `${baseUrl}?search=${req.query.search}` : baseUrl;
+};
+
+router.get('/', (req, res) => Rx.Observable.create((observer) => {
+    console.log(getUrl(url, req));
+    request(
+        getUrl(url, req),
         {
-          method: 'GET',
-          json: true,
+            method: 'GET',
+            json: true,
         },
         (error, response, body) => {
-          if (error) {
-            observer.error(error);
-          } else if (response.statusCode >= 500) {
-            observer.error(response);
-          } else if (response.statusCode === 404) {
-            observer.next(null);
-          } else if (response.statusCode >= 400 && response.statusCode < 500) {
-            observer.next({
-              statusCode: body.statusCode,
-              code: body.code,
-            });
-          } else {
-            observer.next(body);
-          }
+            if (error) {
+                observer.error(error);
+            } else if (response.statusCode >= 500) {
+                observer.error(response);
+            } else if (response.statusCode === 404) {
+                observer.next(null);
+            } else if (response.statusCode >= 400 && response.statusCode < 500) {
+                observer.next({
+                    statusCode: body.statusCode,
+                    code: body.code,
+                });
+            } else {
+                observer.next(body);
+            }
 
-          observer.complete();
+            observer.complete();
         },
-      );
-    })
+    );
+})
     .subscribe(
-      response => res.send(response.results),
-      err => res.status(500).send(err.message),
+        response => res.send(response.results),
+        err => res.status(500).send(err.message),
     ));
+
 
 module.exports = router;
