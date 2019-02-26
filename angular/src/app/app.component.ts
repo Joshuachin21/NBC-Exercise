e849c4989adfd907a610497f00305d721174aaa1
Joshua Chin <joshua.chin21@gmail.com>
2019-02-26 06:07:07 -0800

    2. updated component to watch and pass input value to service and render response. updated service to handle query-string parameter

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { 
+import {
   Component,
-  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ElementRef
 } from '@angular/core';
-import { StarWarsService } from './starwars.service';
+import {fromEvent} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
+import {StarWarsService} from './starwars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   people: any[];
+  @ViewChild('searchInput') searchInput: ElementRef;
 
-  constructor(private swService: StarWarsService) {}
+  constructor(private swService: StarWarsService) {
+  }
 
-  ngOnInit() {
-    this.swService.getPeople().subscribe(result => {
-      this.people = result;
-    })
+  ngAfterViewInit() {
+    fromEvent(this.searchInput.nativeElement, 'input').pipe(
+      map(event => event.target.value),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(q => this.swService.getPeople(q))
+    ).subscribe(
+      result => {
+        this.people = result;
+      }
+    );
   }
 }
--- angular/src/app/starwars.service.ts
+++ angular/src/app/starwars.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class StarWarsService {
+  apiBaseUrl = '/API/v1/star-wars';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  getPeople() {
-    return this.http.get<any>('/API/v1/star-wars');
+  getPeople(searchStr) {
+    return this.http.get<any>(`${this.apiBaseUrl}/?search=${searchStr}`);
   }
 
 }
